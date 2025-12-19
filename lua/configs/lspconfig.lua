d03b1c4087cd4fb89b0a44d1e6c810a816b12405
Amodh Dhakal <amodhakal@gmail.com>
2025-12-19 12:16:11 -0500

    Added zig LSP

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,16 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "zls" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
+
+vim.lsp.config("zls", {
+  settings = {
+    zls = {
+      enable_inlay_hints = true,
+      enable_snippets = true,
+      warn_style = true,
+    },
+  },
+})
