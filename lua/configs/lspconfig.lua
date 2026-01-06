9ad1c96f363e2fe601cd1b18d8884811d261fa78
Amodh Dhakal <amodhakal@gmail.com>
2026-01-06 01:36:07 -0500

    Setup emmet

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -5,7 +5,12 @@ vim.lsp.config("ts_ls", {
   root_markers = { "package.json", "tsconfig.json", ".git" },
 })
 
-local servers = { "html", "cssls", "ts_ls" }
+vim.lsp.config("emmet_language_server", {
+  filetypes = { "html", "css", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  root_markers = { "package.json" },
+})
+
+local servers = { "html", "cssls", "ts_ls", "emmet_language_server" }
 
 vim.lsp.enable(servers)
 -- read :h vim.lsp.config for changing options of lsp servers
