eb84a1138fcb967c898aa21a556e2f8d6686b5ef
Amodh Dhakal <amodhakal@gmail.com>
2026-01-06 18:45:34 -0500

    Cleaned up lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,16 +1,10 @@
 require("nvchad.configs.lspconfig").defaults()
 
-vim.lsp.config("ts_ls", {
-  filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-  root_markers = { "package.json", "tsconfig.json", ".git" },
-})
-
-vim.lsp.config("emmet_language_server", {
-  filetypes = { "html", "css", "javascript", "javascriptreact", "typescript", "typescriptreact" },
-  root_markers = { "package.json" },
-})
-
-local servers = { "html", "cssls", "ts_ls", "emmet_language_server" }
+local servers = {
+  "html",
+  "cssls",
+  "ts_ls",
+  "emmet_language_server",
+}
 
 vim.lsp.enable(servers)
--- read :h vim.lsp.config for changing options of lsp servers
